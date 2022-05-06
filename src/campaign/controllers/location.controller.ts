3d8926b7be1cba330ada04487b86c0efc27c0fcb
kawaki122 <kawaki122@gmail.com>
2022-05-06 06:09:36 +0500

    location api added

--- src/campaign/controllers/location.controller.ts
+++ src/campaign/controllers/location.controller.ts
@@ -1,20 +1,32 @@
-import { Body, Controller, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { LocationDto } from '../dto/location.dto';
 import { LocationService } from '../services/location.service';
 import { LocationUpdateDto } from '../dto/location.update.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('location')
 export class LocationController {
-    constructor(
-        private readonly locationService: LocationService
-    ) { }
+  constructor(private readonly locationService: LocationService) {}
 
-    @Patch()
-    addLocation(@Body() body: LocationDto) {
-        return this.locationService.upsertLocation(body);
-    }
-    @Post("save")
-    updateLocation(@Body() body: LocationUpdateDto) {
-        this.locationService.saveLocationChanges(body)
-    }
+  @Get('getClientLocations')
+  getClientLocations(@Request() req) {
+    return this.locationService.getClientLocations('6223771f4b78b76e16690b2f');
+  }
+
+  @Patch()
+  addLocation(@Body() body: LocationDto) {
+    return this.locationService.upsertLocation(body);
+  }
+  @Post('save')
+  updateLocation(@Body() body: LocationUpdateDto) {
+    this.locationService.saveLocationChanges(body);
+  }
 }
--- src/campaign/services/location.service.ts
+++ src/campaign/services/location.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Brand } from 'src/brand/schemas/brand.schema';
+import { locationEnum } from '../dto/constants';
 import { LocationDto } from '../dto/location.dto';
 import { LocationUpdateDto } from '../dto/location.update.dto';
 import { Location, LocationDocument } from '../schemas/location.schema';
@@ -16,6 +18,21 @@ export class LocationService {
 
   getByCompaign(id) {
     return this.locationModel.find({ campaign: id }).exec();
+  }
+
+  async getClientLocations(clientId) {
+    const locations = await this.locationModel
+      .find({ status: locationEnum.ACTIVE })
+      .populate<{ campaign: { brand: Brand } }>({
+        path: 'campaign',
+        populate: { path: 'brand' },
+      })
+      .orFail()
+      .exec();
+    console.log(
+      locations.filter((item) => item.campaign.brand.client === clientId),
+    );
+    return locations.filter((item) => item.campaign.brand.client === clientId);
   }
 
   async upsertLocation(location: LocationDto) {
@@ -41,11 +58,10 @@ export class LocationService {
 
   async saveLocationChanges(payload: LocationUpdateDto) {
     const result = await this.reviewService.updateReviews(payload.reviews);
-    return this.updateLocation(payload)
+    return this.updateLocation(payload);
   }
 
   updateLocation(payload: LocationUpdateDto) {
-    
     return this.locationModel.findByIdAndUpdate(payload._id, {
       address: payload.address,
       campaign: payload.campaign,
